777e9955abedc585659a2630cdabfa8b9e6070ec
Jenit Joseph Jose <147470538+jenit-tech@users.noreply.github.com>
2025-02-11 14:12:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-const PORT =  process.env.PORT 
+const PORT = 5555 || process.env.PORT 
 
 app.get("/",(request,response)=>{
   response.json({
